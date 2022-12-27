852fe50c74add76fc432d431b036d844b7f49fa3
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2022-12-28 07:49:18 +0900

    laravel8->9

--- contents/laravel/routes/web.php
+++ contents/laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +14,5 @@ use App\Models\User;
 */
 
 Route::get('/', function () {
-    dd(User::first());
     return view('welcome');
 });
